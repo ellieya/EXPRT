9bd1dba5be9ce1daf988b682f4b94ec407c900b4
Chris Harris <42703603+harris-c@users.noreply.github.com>
2019-11-18 14:45:56 -0500

    Add files via upload

--- backend/DB_UTILS.cs
+++ backend/DB_UTILS.cs
@@ -2,11 +2,30 @@
 using System.Collections.Generic;
 using MySql.Data;
 using MySql.Data.MySqlClient;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace EPRT
 {
     class DB_UTILS
     {
+        static string ComputeSha256Hash(string rawData)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
         public static void Select(string amount, string table, string col_name)
         {
 
@@ -36,7 +55,7 @@ namespace EPRT
 
         }
 
-        public static void Insert(string date, string reason, string type, int manager_id, string ssn)
+        public static void Insert(string date, string reason, string type, int manager_id, string ssn, decimal price)
         {
 
             string connect = "server=localhost;database=expense_report_sys;uid=root;pwd=Virfidelis2016!;";
@@ -47,10 +66,11 @@ namespace EPRT
 
                 using (MySqlCommand cmd = conn.CreateCommand())
                 {
+
                     string x = String.Format(
                         "INSERT INTO reports " +
-                        "(ssn, manager_id, expense_type, expense_reason, date_filed) " +
-                        "VALUES ('{0}', '{1}', '{2}', '{3}', '{4}' )", ssn, manager_id, type, reason, date);
+                        "(ssn, manager_id, expense_type, expense_reason, date_filed, price) " +
+                        "VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', {5} )", ComputeSha256Hash(ssn), manager_id, type, reason, date, price);
 
                     cmd.CommandText = x;
                     cmd.ExecuteNonQuery();
@@ -84,6 +104,42 @@ namespace EPRT
             }
 
         }
+
+        public static string Login(string username, string password)
+        {
+
+            string connect = "server=localhost;database=expense_report_sys;uid=root;pwd=Virfidelis2016!;";
+
+            using (var conn = new MySqlConnection(connect))
+            {
+                conn.Open();
+
+                using (MySqlCommand cmd = conn.CreateCommand())
+                {
+                    string x = String.Format(
+                        "SELECT username, password " +
+                        "FROM users WHERE username = '{0}' AND password ='{1}'",username, ComputeSha256Hash(password));
+
+                    cmd.CommandText = x;
+
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return password;
+                        }
+                        else
+                        {
+                            return "ERROR!";
+                        }
+                    }
+
+                }
+
+            }
+
+        }
+
     }
     
    
